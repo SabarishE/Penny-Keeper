14f67a0004c7525db98d5923f38f971a59fd34d7
SabarishE <sabarishtrblzr@gmail.com>
2021-10-04 21:42:54 +0530

    custodians-list

--- routes/workerRoute.js
+++ routes/workerRoute.js
@@ -97,6 +97,26 @@ else{
 })
 
 
+// ----------getting all custodians------
+
+wrouter.get("/allcustodians",async(req,res)=>{
+
+    try{
+        const custodians=await Custodian.find({},{_id:0,passwordHash:0,record:0,email:0,role:0,tempString:0,fundinbox:0});
+
+        // console.log("custodians found",custodians)
+        res.send({msg:"all custodians retreived",data:custodians})
+    }
+    catch(err){
+        console.log("error in finding all custodians")
+        res.status(500).send({msg:"error in finding all custodians",error:err})
+    }
+   
+
+
+
+})
+
 
 // -----Password reset flow starts-------
 
