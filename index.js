aa0cf458d17661deea717e11aadbe9727209ce33
SabarishE <sabarishtrblzr@gmail.com>
2021-11-24 18:21:50 +0530

    index.js

--- index.js
+++ index.js
@@ -5,16 +5,10 @@ import cors from "cors";
 import dotenv from 'dotenv'
 dotenv.config()
 
-// const express = require("express") ;
-// const mongoose=require("mongoose");
-// const cors =require("cors");
-
 import { wrouter } from "./routes/workerRoute.js";
 import { trouter } from "./routes/transactionRoute.js";
 
-
-
- const app=express();
+const app=express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
@@ -24,8 +18,7 @@ app.listen(PORT,console.log("server 🚀 started on port >>>",PORT));
 
 
 
-//  const url= process.env.MONGODB_URI || "mongodb://localhost/pennykeeper";
-const url="mongodb+srv://SabarishE:sabarishe@cluster0.eeimf.mongodb.net/penny-keeper";
+const url=process.env.MONGODB_URI;
 
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true}).catch(error => console.log("mongoose error on initial connection >>>",error));
 
@@ -33,9 +26,6 @@ const dbconnection=mongoose.connection;
 
 dbconnection.on("open",()=>console.log("MongoDB in connected"));
 dbconnection.on("error",()=>console.log("mongoose error after initial connection"))
-
-
-
 
 app.get("/",(req,res)=>{
     res.send("Test request for penny keeper");
